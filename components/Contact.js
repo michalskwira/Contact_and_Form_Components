fb539b1f3a4e3e0db832934befbffa3e5ce25017
Miłosz Panas <miloszpanas@gmail.com>
2018-06-21 11:20:03 +0200

    Add very basic styling styling

--- components/Contact.js
+++ components/Contact.js
@@ -10,10 +10,12 @@ var Contact = React.createClass({
           className: 'contactImage',
           src: 'https://image.flaticon.com/icons/svg/121/121956.svg'
         }),
-        React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
-        React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
-        React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
-          this.props.item.email
+        React.createElement('div', {className: 'contactInfoWrapper'},
+          React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
+          React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
+          React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
+            this.props.item.email
+          )
         )
       )
     )
--- components/ContactForm.js
+++ components/ContactForm.js
@@ -10,16 +10,19 @@ var ContactForm = React.createClass({
           type: 'text',
           placeholder: 'Imię',
           value: this.props.contact.firstName,
+          className: 'dataInput',
         }),
         React.createElement('input', {
           type: 'text',
           placeholder: 'Nazwisko',
           value: this.props.contact.lastName,
+          className: 'dataInput',
         }),
         React.createElement('input', {
           type: 'text',
           placeholder: 'Email',
           value: this.props.contact.email,
+          className: 'emailInput',
         }),
         React.createElement('button', {type: 'submit'}, "Add contact")
       )
